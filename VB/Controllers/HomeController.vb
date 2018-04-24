4861efb1ca2ab3bcf05c0e1a1eb545632bc0245d
DevExpressExampleBot <fakeEmail>
2018-04-24 15:43:05 +0300

    Source auto update [skip ci]

--- VB/Controllers/HomeController.vb
+++ VB/Controllers/HomeController.vb
@@ -1,65 +1,66 @@
 ﻿Imports System
 Imports System.IO
 Imports System.Web
-Imports System.Web.Mvc
 Imports DevExpress.XtraPrinting
 Imports DevExpress.XtraPrintingLinks
-Imports DevExpress.Web.Mvc
+Imports CS.Model
 
-Public Class HomeController
-    Inherits Controller
+Namespace CS.Controllers
+    Public Class HomeController
+        Inherits Controller
 
-    Public Function Index() As ActionResult
-        Return View(New MyViewModel With {.Categories = MyModel.GetCategories(), .Products = MyModel.GetProducts(0)})
-    End Function
+        Public Function Index() As ActionResult
+            Return View(New MyViewModel With { _
+                .Categories = MyModel.GetCategories(), _
+                .Products = MyModel.GetProducts(0) _
+            })
+        End Function
+        Public Function GridViewPartialCategories() As ActionResult
+            Return PartialView(MyModel.GetCategories())
+        End Function
+        Public Function GridViewPartialProducts() As ActionResult
+            Return PartialView(MyModel.GetProducts(Request.Params("MasterRowKey")))
+        End Function
 
-    Public Function GridViewPartialCategories() As ActionResult
-        Return PartialView(MyModel.GetCategories())
-    End Function
+        Public Function ExportTo(ByVal MasterRowKey As Integer) As ActionResult
+            Dim ps As New PrintingSystem()
 
-    Public Function GridViewPartialProducts() As ActionResult
-        Return PartialView(MyModel.GetProducts(Request.Params("MasterRowKey")))
-    End Function
+            Dim link1 As New PrintableComponentLink(ps)
+            Dim categoriesGridSettings As New GridViewSettings()
+            categoriesGridSettings.Name = "gvCategories"
+            categoriesGridSettings.KeyFieldName = "CategoryID"
+            categoriesGridSettings.Columns.Add("CategoryID")
+            categoriesGridSettings.Columns.Add("CategoryName")
+            categoriesGridSettings.Columns.Add("Description")
+            link1.Component = GridViewExtension.CreatePrintableObject(categoriesGridSettings, MyModel.GetCategories())
 
-    Public Function ExportTo(ByVal MasterRowKey As Integer) As ActionResult
-        Dim ps As New PrintingSystem()
+            Dim link2 As New PrintableComponentLink(ps)
+            Dim productsGridSettings As New GridViewSettings()
+            productsGridSettings.Name = "gvProducts"
+            productsGridSettings.KeyFieldName = "ProductID"
+            productsGridSettings.Columns.Add("ProductID")
+            productsGridSettings.Columns.Add("ProductName")
+            productsGridSettings.Columns.Add("CategoryID")
+            productsGridSettings.Columns.Add("UnitPrice")
+            link2.Component = GridViewExtension.CreatePrintableObject(productsGridSettings, MyModel.GetProducts(MasterRowKey))
 
-        Dim link1 As New PrintableComponentLink(ps)
-        Dim categoriesGridSettings As New GridViewSettings()
-        categoriesGridSettings.Name = "gvCategories"
-        categoriesGridSettings.KeyFieldName = "CategoryID"
-        categoriesGridSettings.Columns.Add("CategoryID")
-        categoriesGridSettings.Columns.Add("CategoryName")
-        categoriesGridSettings.Columns.Add("Description")
-        link1.Component = GridViewExtension.CreatePrintableObject(categoriesGridSettings, MyModel.GetCategories())
+            Dim compositeLink As New CompositeLink(ps)
+            compositeLink.Links.AddRange(New Object() { link1, link2 })
+            compositeLink.CreateDocument()
 
-        Dim link2 As New PrintableComponentLink(ps)
-        Dim productsGridSettings As New GridViewSettings()
-        productsGridSettings.Name = "gvProducts"
-        productsGridSettings.KeyFieldName = "ProductID"
-        productsGridSettings.Columns.Add("ProductID")
-        productsGridSettings.Columns.Add("ProductName")
-        productsGridSettings.Columns.Add("CategoryID")
-        productsGridSettings.Columns.Add("UnitPrice")
-        link2.Component = GridViewExtension.CreatePrintableObject(productsGridSettings, MyModel.GetProducts(MasterRowKey))
+            Dim result As FileStreamResult = CreateExcelExportResult(compositeLink)
+            ps.Dispose()
 
-        Dim compositeLink As New CompositeLink(ps)
-        compositeLink.Links.AddRange(New Object() {link1, link2})
-        compositeLink.CreateDocument()
+            Return result
+        End Function
 
-        Dim result As FileStreamResult = CreateExcelExportResult(compositeLink)
-        ps.Dispose()
-
-        Return result
-    End Function
-
-    Private Function CreateExcelExportResult(ByVal link As CompositeLink) As FileStreamResult
-        Dim stream As New MemoryStream()
-        link.PrintingSystem.ExportToXls(stream)
-        stream.Position = 0
-        Dim result As New FileStreamResult(stream, "application/xls")
-        result.FileDownloadName = "MyData.xls"
-        Return result
-    End Function
-
-End Class
+        Private Function CreateExcelExportResult(ByVal link As CompositeLink) As FileStreamResult
+            Dim stream As New MemoryStream()
+            link.PrintingSystem.ExportToXls(stream)
+            stream.Position = 0
+            Dim result As New FileStreamResult(stream, "application/xls")
+            result.FileDownloadName = "MyData.xls"
+            Return result
+        End Function
+    End Class
+End Namespace
--- VB/My Project/AssemblyInfo.vb
+++ VB/My Project/AssemblyInfo.vb
@@ -1,24 +1,23 @@
-﻿Imports System
-Imports System.Reflection
+﻿Imports System.Reflection
+Imports System.Runtime.CompilerServices
 Imports System.Runtime.InteropServices
-
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
-
-' Review the values of the assembly attributes
-<Assembly: AssemblyTitle("VB")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("VB")> 
-<Assembly: AssemblyCopyright("Copyright ©  2012")> 
-<Assembly: AssemblyTrademark("")> 
-
+<Assembly: AssemblyTitle("CS")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyConfiguration("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("CS")>
+<Assembly: AssemblyCopyright("Copyright ©  2011")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+' Setting ComVisible to false makes the types in this assembly not visible 
+' to COM components.  If you need to access a type in this assembly from 
+' COM, set the ComVisible attribute to true on that type.
 <Assembly: ComVisible(False)>
-
 ' The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("197b6361-16c8-4c60-9cbe-75c1c9d16ed2")> 
-
+<Assembly: Guid("f5b95a65-1716-429e-9c68-7e46fc374c1c")>
 ' Version information for an assembly consists of the following four values:
 '
 '      Major Version
@@ -26,9 +25,7 @@ Imports System.Runtime.InteropServices
 '      Build Number
 '      Revision
 '
-' You can specify all the values or you can default the Build and Revision Numbers 
+' You can specify all the values or you can default the Revision and Build Numbers 
 ' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.*")> 
-
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
